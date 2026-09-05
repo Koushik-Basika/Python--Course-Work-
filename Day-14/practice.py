4d80fbb38990817c66a02db30a73ba399ddbcd59
Koushik-Basika <koushikbasika@gmail.com>
2026-09-05 15:56:27 +0530

    forwithelse and whileloop

---
+++ Day-14/practice.py
@@ -0,0 +1,32 @@
+'''
+n=int(input())
+for i in range(1,n+1):
+    print(i,end=" ")
+'''
+'''
+n=int(input())
+for i in range(1,n+1):
+    if i%2==0:
+        print(i,end=" ")
+'''
+'''
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    sum=sum+1
+print(sum)    
+'''
+'''
+n=int(input())
+for i in range(1,n+1):
+    if i%2!=0:
+       print(i,end=" ")
+'''
+'''
+n=int(input())
+fact=1
+for i in range(1, n + 1):
+    fact=fact*i
+print(fact)     
+'''
+
