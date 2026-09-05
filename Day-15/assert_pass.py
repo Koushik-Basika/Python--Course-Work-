af1728767424ddaffb9e0efd8f7609f32a7bf1e2
Koushik-Basika <koushikbasika@gmail.com>
2026-09-05 15:57:09 +0530

    assert_pass and patterns

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,14 @@
+'''
+n = int(input("Enter a number: "))
+assert n >= 0
+print("Number is positive")
+'''
+
+'''
+n = int(input("Enter a number: "))
+if n > 0:
+    pass
+else:
+    print("Number is not positive")
+print("Program completed")
+'''
