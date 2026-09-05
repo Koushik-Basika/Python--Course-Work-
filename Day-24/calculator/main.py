8d3ee90d840e58fee9a8693d57a9377828a5c6c4
Koushik-Basika <koushikbasika@gmail.com>
2026-09-05 16:01:14 +0530

    ATM and Calculator

---
+++ Day-24/calculator/main.py
@@ -0,0 +1,29 @@
+'''
+import logic 
+logic.add(2,3)
+logic.sub(2,3)
+logic.mul(2,3)
+logic.div(2,3)
+logic.exp(2,3)
+logic.rem(2,3)
+
+import logic as lg                         # giving alias name for given python file(.py)
+lg.add(2,3)
+lg.sub(2,3)
+lg.mul(2,3)
+lg.div(2,3)
+lg.exp(2,3)
+lg.rem(2,3)
+
+from logic import add,sub
+add(4,5)
+sub(8,7)
+
+from logic import *
+add(2,3)
+sub(2,3)
+mul(2,3)
+div(2,3)
+exp(2,3)
+rem(2,3)
+'''
